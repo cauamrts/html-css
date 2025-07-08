12928f1cf69deff704abbd2aaa9bbd64484da53c
Cauã Martins <cauamrts777@gmail.com>
2025-07-07 21:08:14 -0300

    formulario 3

--- ex025/cadastro.php
+++ ex025/cadastro.php
@@ -1,2 +1,1 @@
-Esse é a pagina do PHP mais no momento
-não serve para nada por que não estamos estudando PHP, serve só de exemplo.
+Esse é a pagina do PHP. No momento não serve para nada, por que não estamos estudando PHP. serve só de exemplo.
